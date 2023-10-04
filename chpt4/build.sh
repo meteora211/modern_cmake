8d54a6f02a1a03fded1dad95a18c1f973fb74dda
zhaorui chen <czhaor@gmail.com>
2023-10-04 14:54:45 +0800

    update target.

--- chpt4/build.sh
+++ chpt4/build.sh
@@ -1,4 +1,6 @@
 cmake -B `pwd`/build -S `pwd` --graphviz=`pwd`/build/test.dot
-cmake --build `pwd`/build
+# cmake --build `pwd`/build --target destination
+cmake --build `pwd`/build --target all
+cmake --build `pwd`/build --target efa
 
-cat `pwd`/build/test.dot
+# cat `pwd`/build/test.dot
---
+++ chpt4/empty.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+  std::cout << "empty" << std::endl;
+  return 0;
+}
