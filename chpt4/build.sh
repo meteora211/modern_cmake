6a04538bee641bb3fb344194a287aec8d2e6993f
zhaorui chen <czhaor@gmail.com>
2023-10-07 23:14:31 +0800

    update genexes

--- chpt4/build.sh
+++ chpt4/build.sh
@@ -3,4 +3,5 @@ cmake -B `pwd`/build -S `pwd` --graphviz=`pwd`/build/test.dot
 cmake --build `pwd`/build --target all
 cmake --build `pwd`/build --target efa
 
-# cat `pwd`/build/test.dot
+cat `pwd`/build/test.dot
+cat `pwd`/build/output_log
