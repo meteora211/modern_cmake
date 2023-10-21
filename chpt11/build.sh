005ca4fed572d9f6f13ff70f824141ccf98510a0
zhaorui chen <czhaor@gmail.com>
2023-10-21 21:35:39 +0800

    update install package.

--- chpt11/build.sh
+++ chpt11/build.sh
@@ -2,8 +2,9 @@ cmake -B `pwd`/build -S `pwd`
 cmake --build `pwd`/build
 cmake --install `pwd`/build
 
-tree ./install
 
 # cat ./build/cmake/CalcTargets.cmake
 
-cmake -B `pwd`/find_package/build -S `pwd`/find_package -DCMAKE_PREFIX_PATH=`pwd`/install
+cmake -B `pwd`/find_package/build -S `pwd`/find_package -DCMAKE_PREFIX_PATH=`pwd`/../install/
+
+tree ../install
